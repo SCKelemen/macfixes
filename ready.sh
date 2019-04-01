51cc42e9f57b46d46a2b6cd0d603df5321743761
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2019-04-01 11:25:07 -0400

    Create ready.sh

---
+++ ready.sh
@@ -0,0 +1,10 @@
+cd ~
+mkdir Screenshots
+cd ./Screenshots
+git clone https://github.com/SCKelemen/ss.git ./ 
+defaults write com.apple.screencapture location ~/Screenshots
+
+cd ~
+mkdir Code 
+
+defaults write com.apple.finder AppleShowAllFiles YES
