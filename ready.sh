4ec742eac28b78965e52f4211c6894f7e1367b47
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2019-04-02 11:58:30 -0400

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -1,10 +1,20 @@
+
+echo Setup Screenshots folder
 cd ~
 mkdir Screenshots
 cd ./Screenshots
 git clone https://github.com/SCKelemen/ss.git ./ 
 defaults write com.apple.screencapture location ~/Screenshots
 
+echo Setup Code directory
 cd ~
 mkdir Code 
 
+echo Setup Papers directory
+cd ~ 
+mkdir Papers
+cd ./Papers
+git clone https://github.com/SCKelemen/resources.git ./
+
+echo Show Hidden Files in Finder
 defaults write com.apple.finder AppleShowAllFiles YES
